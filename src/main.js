6e6311c8c02ed5351450e54db3cd6ae8eed6f8e1
Daniel Huniewicz <huniewiczdaniel@gmail.com>
2025-08-05 11:30:00 +0200

    Add base vuetify 2

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
-import { createApp } from 'vue'
+/**
+ * main.js
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
+
+// Plugins
+import { registerPlugins } from '@/plugins'
+
+// Components
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
 
-loadFonts()
+// Composables
+import { createApp } from 'vue'
 
-createApp(App)
-  .use(vuetify)
-  .mount('#app')
+// Styles
+import 'unfonts.css'
+
+const app = createApp(App)
+
+registerPlugins(app)
+
+app.mount('#app')
